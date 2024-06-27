67ba948269bacbfe628e87b1e5f7f5af36e61de0
DCS <dcsh2o@gmail.com>
2024-06-27 19:23:28 -0300

    Exercicio Vogais v.2

--- vogais/vogais.py
+++ vogais/vogais.py
@@ -1,7 +1,24 @@
+#Função cacavogais(): Essa função conta o número de vogais 
+# em uma string (sequência de caracteres) digitada pelo usuário.
+#
+
 def cacavogais():
+    #Variáveis:
+
+# i: um contador que itera sobre cada caractere da string.
+# j: um contador que soma o número de vogais encontradas.
+# string: a string digitada pelo usuário.
+
     i = 0
     j = 0
+
+    
     string = str (input("Digite alguma coisa: "))
+#A função pede ao usuário que digite uma string
+#  com input("Digite alguma coisa: ").
+
+
+
     for i in string:
         if (i == 'A' or i == 'a'
         or i == 'E' or i == 'e'
@@ -10,5 +27,5 @@ def cacavogais():
         or i == 'U' or i == 'u'):
              j+=1
     print("vogais: ", j)
-
+#Em resumo, a função cacavogais() conta o número de vogais em uma string digitada pelo usuário e imprime o resultado.
 cacavogais()
