c29b568ed11e2ca3e16ac10fd15cbd77ed13e36f
DCS <dcsh2o@gmail.com>
2024-07-01 21:11:22 -0300

    Exercicio Funcoes v.1

--- Funcoes/funcao.py
+++ Funcoes/funcao.py
@@ -3,13 +3,25 @@
 
 
 def saudacao():
-    print('Olá, seja bem vindo(a) saldação!')
+    print('Olá, seja bem vindo(a) Digite sua nota de 1 a 10!')
 
 saudacao()
 
 def soma (a,b):
-    a=1
-    b=2
-    resultado= a+b
+    a= int(input("Nota 1:"))
+    b = int(input("Nota 2:"))
+    nota= 8
     
+    resultado= a+b
+    print(f"valor {resultado} esperado")
+    
+    if nota<=resultado:
+        print("Seu Time é Azul")
+        
+    else:
+        print("Seu time é Vermelho")
+
     return resultado
+
+print(soma(0,0))
+
