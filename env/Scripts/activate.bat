49554b6a4dc43ffb3b2e3b7691b875b3dd67130c
DCS <dcsh2o@gmail.com>
2024-07-08 15:09:35 -0300

    Desafio Final V.1

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\Aula_QA\aula\env
+set VIRTUAL_ENV=C:\Users\DCS\Desktop\QA-IJJ\aula\env
 
 if not defined PROMPT set PROMPT=$P$G
 
